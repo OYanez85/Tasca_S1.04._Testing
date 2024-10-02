6f38d96fe7048fc804f3cc3d088b3eb8c45a97d3
OYanez85 <oscar.yanez.feijoo@gmail.com>
2024-10-02 13:34:55 +0200

    Added files to Level_1

---
+++ Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    private String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return title.equals(book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode();
+    }
+}
